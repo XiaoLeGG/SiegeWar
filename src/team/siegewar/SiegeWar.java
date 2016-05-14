c5667971be46879d87c45b77f43f63be7ea47b18
Administrator <Administrator@192.168.1.102>
2016-05-14 23:19:40 +0800

    Update 2016.5.14

--- src/team/siegewar/SiegeWar.java
+++ src/team/siegewar/SiegeWar.java
@@ -1,11 +1,16 @@
 package team.siegewar;
 
+import java.io.File;
+
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SiegeWar extends JavaPlugin implements Listener{
 	public void onEnable(){
 		getLogger().info("攻城战争已开启");
+		this.getDataFolder().mkdirs();
+		if (!new File(getDataFolder(),"config.yml").exists()) this.saveResource("config", false);
+		new File(getDataFolder(),"\\Games").mkdirs();
 	}
 	public void onLoad(){
 		getLogger().info("攻城战争已加载");
