3481ea32cb068d893a35cf974ec00f6d3ab7ffad
Austin <786071433@qq.com>
2016-05-14 23:22:08 +0800

    Update 2016.5.14 23.22

---
+++ src/team/siegewar/GameManager/GameLoad.java
@@ -0,0 +1,15 @@
+package team.siegewar.GameManager;
+
+import org.bukkit.event.Listener;
+
+import team.siegewar.SiegeWar;
+
+public class GameLoad implements Listener{
+	SiegeWar siegeWar;
+	public GameLoad(SiegeWar siegeWar){
+		this.siegeWar = siegeWar;
+	}
+	public void load(String game){
+	
+	}
+}
