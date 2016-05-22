8bbad1f212d89baf60e99d411416bb2bccb32acc
Nodak_Lzc <1270410764@qq.com>
2016-05-22 14:04:55 +0800

    2016年5月22日14:04:54

--- src/team/siegewar/Commands.java
+++ src/team/siegewar/Commands.java
@@ -14,7 +14,36 @@ public class Commands implements Listener{
 	@EventHandler
 	public void onPlayerCommand(PlayerCommandPreprocessEvent e){
 		Player p=e.getPlayer();
-		p.sendMessage(e.getMessage());
+		if(e.getMessage().startsWith("/siegewar")||e.getMessage().startsWith("/sw")){
+			String msg=e.getMessage();
+			String[] args=msg.split(" ");
+			if(args.length==1){
+				p.sendMessage("§c§l攻城战争 §e§l0.1_alpha:                    ");
+				p.sendMessage("§e§o /siegewar   help 列出帮助                       ");
+				p.sendMessage("§e§o /siegewar create [游戏名字] 创建一个游戏");
+				p.sendMessage("§c§lby: §a§l§oa_austin,Nodak_Lzc");
+				return;
+			}
+			if(args[1].equals("help")){
+				p.sendMessage("                §c§l攻城战争                         ");
+				p.sendMessage("§e§o /siegewar   help 列出帮助                       ");
+				p.sendMessage("§e§o /siegewar create [游戏名字] 创建一个游戏");
+				p.sendMessage("§c§lby: §a§l§oa_austin,Nodak_Lzc");
+				return;
+			}
+			if(args[1].equals("create")){
+				if(args.length<3||args.length>3){
+					p.sendMessage("§b§l正确用法: /siegewar create [游戏名字] 创建一个游戏");
+					return;
+				}else{
+					String name=args[2];
+					p.sendMessage("§6§l你成功创建了名为: §b§l"+name+" §6§l的游戏");
+					return;
+				}
+			}
+		}
+
 	}
+	
 
 }
--- src/team/siegewar/SiegeWar.java
+++ src/team/siegewar/SiegeWar.java
@@ -1,9 +1,6 @@
 package team.siegewar;
 
-import java.io.File;
-import java.io.IOException;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,17 +12,12 @@ public class SiegeWar extends JavaPlugin implements Listener{
 	GameLoad gameLoad;
 	public void onEnable(){
 		getLogger().info("攻城战争已开启");
-		this.getDataFolder().mkdirs();
 		createFile();
-		config = YamlConfiguration.loadConfiguration(new File(getDataFolder(),"config.yml"));
+		saveDefaultConfig();
 		gameLoad = new GameLoad(this);
-		gameLoad.reload();
 		getServer().getPluginManager().registerEvents(new Commands(this), this);
 	}
 	public void createFile(){
-		if (!new File(getDataFolder(),"config.yml").exists()) this.saveResource("config", false);
-		new File(getDataFolder(),"\\Games").mkdirs();
-		new File(getDataFolder(),"\\PlayerData").mkdirs();
 	}
 	public void onLoad(){
 		getLogger().info("攻城战争已加载");
@@ -33,6 +25,6 @@ public class SiegeWar extends JavaPlugin implements Listener{
 	public void onDisable(){
 		getLogger().info("攻城战争已关闭");
 	}
-	public FileConfiguration getConfig(){ return this.config; }
+	public FileConfiguration getConfig(){ return getConfig(); }
 	public FileConfiguration getGames(){ return this.games; }
 }
